043d646093366a1681d9215a046e03995d288c45
Greg Rubin <grubin@catchpoint.com>
2017-06-21 17:16:01 -0400

    Solutions provied

--- JavaScript/ArrayProblemSet/problemset.js
+++ JavaScript/ArrayProblemSet/problemset.js
@@ -1,9 +1,13 @@
 function printReverse(arr) {
-  var newArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    newArr.unshift(arr[i]);
+  // var newArr = [];
+  // for (var i = 0; i < arr.length; i++) {
+  //   newArr.unshift(arr[i]);
+  // }
+  // return newArr;
+
+  for(var i = arr.length - 1 ; i >= 0; i--) {
+    console.log(arr[i]);
   }
-  return newArr;
 }
 
 function isUniform(arr) {
@@ -17,11 +21,16 @@ function isUniform(arr) {
 }
 
 function sumArray(arr) {
-  sum = 0
-  for(var i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-  return sum;
+//   sum = 0
+//   for(var i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum;
+  var total = 0;
+  arr.forEach(function(el) {
+    total += el;
+  })
+  return total;
 }
 
 function max(arr) {
