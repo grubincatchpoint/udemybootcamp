38ad171f5abfe54edd2346c0e1297388d4627f14
Greg Rubin <grubin@catchpoint.com>
2017-06-21 13:39:58 -0400

    added delete to Todo app, also added functions for orgnization

--- JavaScript/ToDoListApp/tdlist.js
+++ JavaScript/ToDoListApp/tdlist.js
@@ -4,12 +4,34 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit") {
   if (input === "list") {
-    console.log(todos);
+    listTodos();
   } else if (input === "new") {
-    var newTodo = prompt("enter new todo");
-    todos.push(newTodo);
-    console.log(todos);
+    addTodo();
+  } else if (input === "delete") {
+    deleteTodo();
   }
   input = prompt("What would you like to do?");
 }
 console.log("OK, you quit the app!");
+
+function listTodos() {
+  console.log("***********");
+    todos.forEach(function(todo, index) {
+      console.log(index + ": "+ todo);
+    });
+    console.log("***********");
+}
+
+function addTodo() {
+  var newTodo = prompt("enter new todo");
+    todos.push(newTodo);
+    console.log("It is now added to your todo list.");
+}
+
+function deleteTodo() {
+  //ask for index of todo to be deleted
+    var inDelete = prompt("Enter the index of todo to delete");
+    //delete that todo
+    todos.splice(inDelete, 1);
+    console.log("Index " + inDelete + " was deleted.");
+}
