4f4df81729a035763e60e92f89815ffb30f04d09
Greg Rubin <grubin@catchpoint.com>
2017-06-22 15:36:17 -0400

    excercise: button that changes background to purple or white

--- JavaScript/Intro to the DOM/domination.js
+++ JavaScript/Intro to the DOM/domination.js
@@ -1,19 +1,49 @@
 //Select and Manipulate
-var h1 = document.querySelector("h1");
-h1.style.color = "purple";
-var highlight = document.getElementById("highlight");
+// var h1 = document.querySelector("h1");
+// h1.style.color = "purple";
+// var highlight = document.getElementById("highlight");
 //Can put #some-class in CSS with properties to change
-highlight.classList.add("some-class");
+// highlight.classList.add("some-class");
 //Remove class
-highlight.classList.remove("some-class");
+// highlight.classList.remove("some-class");
 //toggle
-highlight.classList.toggle("some-class");
-var bolded = document.getElementsByClassName("bolded");
-var tags = document.getElementsByTagName("li");
-var highlightSelector = document.querySelector("#highlight");
-var allLi = document.querySelectorAll("li");
+// highlight.classList.toggle("some-class");
+// var bolded = document.getElementsByClassName("bolded");
+// var tags = document.getElementsByTagName("li");
+// var highlightSelector = document.querySelector("#highlight");
+// var allLi = document.querySelectorAll("li");
 
 //.textContent to change text
 //.innerHTML to change HTML
 //.getAttribute to get attributes
 //.setAttribute to modify attributes of an element
+
+// Add an event listener
+//el.addEventListener("event", function() {});
+var h1 = document.querySelector("h1");
+h1.addEventListener("click", function() {
+  h1.style.background = "orange";
+});
+
+document.querySelector("ul").addEventListener("click", function() {
+  console.log("YOU CLICKED UL!");
+})
+
+var li = document.querySelectorAll("li");
+
+for (var i=0; i < li.length; i++) {
+  li[i].addEventListener("click", function() {
+    this.style.color = "pink";
+  })
+};
+
+var button = document.querySelector("button");
+var purple = false;
+button.addEventListener("click", function() {
+  if(!purple) {
+    document.querySelector("body").style.background = "purple";
+  } else {
+    document.querySelector("body").style.background = "white";
+  }
+  purple = !purple;
+});
