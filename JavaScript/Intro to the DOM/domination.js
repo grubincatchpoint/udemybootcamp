5490a5be52e4565f5910ba851fd57a8f795cdbe4
Greg Rubin <grubin@catchpoint.com>
2017-06-22 13:43:20 -0400

    adding and removing style/class

--- JavaScript/Intro to the DOM/domination.js
+++ JavaScript/Intro to the DOM/domination.js
@@ -2,6 +2,12 @@
 var h1 = document.querySelector("h1");
 h1.style.color = "purple";
 var highlight = document.getElementById("highlight");
+//Can put #some-class in CSS with properties to change
+highlight.classList.add("some-class");
+//Remove class
+highlight.classList.remove("some-class");
+//toggle
+highlight.classList.toggle("some-class");
 var bolded = document.getElementsByClassName("bolded");
 var tags = document.getElementsByTagName("li");
 var highlightSelector = document.querySelector("#highlight");
