c0301e49511d3407403ea9b597195abcf753af06
Greg Rubin <grubin@catchpoint.com>
2017-06-22 13:35:46 -0400

    multiple ways of selecting elements

--- JavaScript/Intro to the DOM/domination.js
+++ JavaScript/Intro to the DOM/domination.js
@@ -1,3 +1,8 @@
 //Select and Manipulate
 var h1 = document.querySelector("h1");
 h1.style.color = "purple";
+var highlight = document.getElementById("highlight");
+var bolded = document.getElementsByClassName("bolded");
+var tags = document.getElementsByTagName("li");
+var highlightSelector = document.querySelector("#highlight");
+var allLi = document.querySelectorAll("li");
