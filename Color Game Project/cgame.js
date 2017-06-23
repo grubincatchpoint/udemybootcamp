2775d21c7cbb2c4271a583413905cd5491d1c866
Greg Rubin <grubin@catchpoint.com>
2017-06-23 10:45:35 -0400

    Made picked color random and added the change colors function

--- Color Game Project/cgame.js
+++ Color Game Project/cgame.js
@@ -7,8 +7,9 @@ var colors = [
   "rgb(255, 0, 255)",
   ];
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDislay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,11 +19,27 @@ for(var i = 0; i < squares.length; i++) {
   //add click listeners to squares
   squares[i].addEventListener("click", function(){
     //grab color of clicked square
-    var clickedColor = this.style.background;
+    var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
-      alert("Correct!");
-    } else {alert("Wrong!");
+      messageDislay.textContent = "Correct!";
+      changeColors(clickedColor);
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDislay.textContent = "Try Again!";
     }
   })
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for(var i = 0; i < squares.length; i++) {
+     //change each color to match given color
+     squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random];
 }
