b650aec080e09d5841793316ff2f5176aa8ea6f1
Greg Rubin <grubin@catchpoint.com>
2017-06-30 12:28:34 -0400

    classes jQuery

--- jQuery/jquerydemo.js
+++ jQuery/jquerydemo.js
@@ -3,5 +3,12 @@
 // $("#third").css("border", "1px solid orange");
 // $("div:first-of-type").css("color", "pink");
 
+// .text jQ = textContent js
+// .html jQ = innerHtml js
+// .attr lets you change attributes in jq
+// .val extracts val from input or change the value.
+// .addClass
+// .removeClass
+// .toggleClass
 var textCommand = $("h1").text();
-var changeText = $("h1").text("SURPRISE!!!!");
+$("img").attr("src", "https://i.ytimg.com/vi/zx0gXM_FkgE/maxresdefault.jpg");
