61ba30d41b276738f957bfe04faf5ff7dfb75592
Greg Rubin <grubin@catchpoint.com>
2017-06-30 14:05:40 -0400

    on() event

--- jQuery/jquerydemo.js
+++ jQuery/jquerydemo.js
@@ -1,14 +1,19 @@
-// $("div").css("background", "purple");
-// $(".highlight").css("width", "200px");
-// $("#third").css("border", "1px solid orange");
-// $("div:first-of-type").css("color", "pink");
-
-// .text jQ = textContent js
-// .html jQ = innerHtml js
-// .attr lets you change attributes in jq
-// .val extracts val from input or change the value.
-// .addClass
-// .removeClass
-// .toggleClass
+ /*
+  $("div").css("background", "purple");
+  $(".highlight").css("width", "200px");
+  $("#third").css("border", "1px solid orange");
+  $("div:first-of-type").css("color", "pink");
+  .text jQ = textContent js
+  .html jQ = innerHtml js
+  .attr lets you change attributes in jq
+  .val extracts val from input or change the value.
+  .addClass
+  .removeClass
+  .toggleClass
+  .click adds a click event listener
+  .keypress adds a keypress listener
+  if keypressVal.which === 13
+  on jQ gives you an event listener with a function
+*/
 var textCommand = $("h1").text();
 $("img").attr("src", "https://i.ytimg.com/vi/zx0gXM_FkgE/maxresdefault.jpg");
