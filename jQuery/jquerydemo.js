62d03b54c32f6e83f56eece69d0681bfe2d0f6b0
Greg Rubin <grubin@catchpoint.com>
2017-06-30 12:14:19 -0400

    text() and html() demo

--- jQuery/jquerydemo.js
+++ jQuery/jquerydemo.js
@@ -1,4 +1,7 @@
-$("div").css("background", "purple");
-$(".highlight").css("width", "200px");
-$("#third").css("border", "1px solid orange");
-$("div:first-of-type").css("color", "pink");
+// $("div").css("background", "purple");
+// $(".highlight").css("width", "200px");
+// $("#third").css("border", "1px solid orange");
+// $("div:first-of-type").css("color", "pink");
+
+var textCommand = $("h1").text();
+var changeText = $("h1").text("SURPRISE!!!!");
