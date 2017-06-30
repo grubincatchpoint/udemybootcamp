9a419f1aaaed0405143c4f5e50239a1f8c616799
Greg Rubin <grubin@catchpoint.com>
2017-06-30 15:13:18 -0400

    added delete method to x/span

--- Todo App jQuery CSS HTML/assets/js/app.js
+++ Todo App jQuery CSS HTML/assets/js/app.js
@@ -2,3 +2,11 @@
 $('li').click(function() {
   $(this).toggleClass("completed");
 });
+
+//Delete when clicking the span
+$("span").click(function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
